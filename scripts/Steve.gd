a5fe75eceec8a5d0e68ec0d6dd37c94ab0539d96
Rodrigo Alfonso <rodri042@gmail.com>
2020-02-05 02:07:39 -0300

    Extracting const

--- scripts/Steve.gd
+++ scripts/Steve.gd
@@ -1,5 +1,7 @@
 extends KinematicBody
 
+const SPEED = 5
+
 var velocity = Vector3(0, 0, 0)
 
 func _ready():
@@ -9,18 +11,18 @@ func _physics_process(delta):
 	if Input.is_action_pressed("ui_right") and Input.is_action_pressed("ui_left"):
 		velocity.x = 0
 	elif Input.is_action_pressed("ui_right"):
-		velocity.x = 5
+		velocity.x = SPEED
 	elif Input.is_action_pressed("ui_left"):
-		velocity.x = -5
+		velocity.x = -SPEED
 	else:
 		velocity.x = lerp(velocity.x, 0, 0.1)
 	
 	if Input.is_action_pressed("ui_up") and Input.is_action_pressed("ui_down"):
 		velocity.z = 0
 	elif Input.is_action_pressed("ui_up"):
-		velocity.z = -5
+		velocity.z = -SPEED
 	elif Input.is_action_pressed("ui_down"):
-		velocity.z = 5
+		velocity.z = SPEED
 	else:
 		velocity.z = lerp(velocity.z, 0, 0.1)
 	
