eff830e4e69ec5ea506ce522f4fedc3f0f8b9752
Rodrigo Alfonso <rodri042@gmail.com>
2020-02-05 02:06:12 -0300

    Adding movement

---
+++ scripts/Steve.gd
@@ -0,0 +1,27 @@
+extends KinematicBody
+
+var velocity = Vector3(0, 0, 0)
+
+func _ready():
+	print("Hello Godot!")
+
+func _physics_process(delta):
+	if Input.is_action_pressed("ui_right") and Input.is_action_pressed("ui_left"):
+		velocity.x = 0
+	elif Input.is_action_pressed("ui_right"):
+		velocity.x = 5
+	elif Input.is_action_pressed("ui_left"):
+		velocity.x = -5
+	else:
+		velocity.x = lerp(velocity.x, 0, 0.1)
+	
+	if Input.is_action_pressed("ui_up") and Input.is_action_pressed("ui_down"):
+		velocity.z = 0
+	elif Input.is_action_pressed("ui_up"):
+		velocity.z = -5
+	elif Input.is_action_pressed("ui_down"):
+		velocity.z = 5
+	else:
+		velocity.z = lerp(velocity.z, 0, 0.1)
+	
+	move_and_slide(velocity)
